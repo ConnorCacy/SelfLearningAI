3d5d331f668f2440e9c42c86e39a89b95bcbc4ab
conno <conno@BASIL>
2022-06-06 23:48:56 -0500

    Add project files.

---
+++ Brain.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+internal class Brain
+{
+    private IntakeSensor _intakeSensor;
+
+    public Brain(IntakeSensor intakeSensor)
+    {
+        _intakeSensor = intakeSensor;
+    }
+
+    internal object Decide(object observations)
+    {
+        throw new NotImplementedException();
+    }
+
+    internal object Observe()
+    {
+        var perceivables = _intakeSensor.GetAllPerceivables();
+        return perceivables;
+    }
+
+    internal void Sleep(object threadsOfDecisions)
+    {
+        throw new NotImplementedException();
+    }
+
+    internal bool IsSleepy(object threadsOfDecisions)
+    {
+        throw new NotImplementedException();
+    }
+
+    internal object Decide(object observations, object threadsOfDecisions)
+    {
+        throw new NotImplementedException();
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿var brain = new Brain(new IntakeSensor { });
+var time2Sleep = false;
+while (true)
+{
+    var observations = brain.Observe();
+    var threadsOfDecisions = brain.Decide(observations);
+    while (!time2Sleep)
+    {
+        observations = brain.Observe();
+        threadsOfDecisions = brain.Decide(observations, threadsOfDecisions);
+        time2Sleep = brain.IsSleepy(threadsOfDecisions);
+    }
+    brain.Sleep(threadsOfDecisions);
+    time2Sleep = false;
+}
